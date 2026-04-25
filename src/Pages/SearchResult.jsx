8e4995155b42c46d9757484831c026e8975484cc
Vivek-Krishna00 <vivekkrishna7985@gmail.com>
2026-04-25 22:03:15 +0530

    fix: resolve case-sensitive import paths for Vercel deployment

--- src/Pages/SearchResult.jsx
+++ src/Pages/SearchResult.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import Layout from "../Components/Layout";
-import VideoList from "../components/VideoList";
+import VideoList from "../Components/VideoList";
 import { fetchVideosWithDetails, normalizeVideoData } from "../utils/api";
 import { useInfiniteScroll } from "../utils/useInfiniteScroll";
 import "./Pages.css";
